e321ba34fc9517de72687c34030c83f13a1a75a1
flomero <49640251+flomero@users.noreply.github.com>
2024-03-21 18:52:33 +0100

    remove unused import

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:32:05 by flfische          #+#    #+#             */
-/*   Updated: 2024/03/21 18:36:57 by flfische         ###   ########.fr       */
+/*   Updated: 2024/03/21 18:51:58 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <limits.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
